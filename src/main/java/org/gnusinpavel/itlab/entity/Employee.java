de89dbd6953933977bd351363cc05e2db8b8f41e
Gnusin Pavel <pavel.gnusin@codeinside.ru>
2015-07-20 22:34:41 +0300

    id сделан Long чтобы были значнеия null

--- src/main/java/org/gnusinpavel/itlab/entity/Employee.java
+++ src/main/java/org/gnusinpavel/itlab/entity/Employee.java
@@ -32,11 +32,11 @@ public class Employee {
         this.gender = gender;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
